a5977c2c609f1e37baa19ba75c6c2c327a632efb
Michael Baldock <michaelb1983@gmail.com>
2024-10-01 09:54:59 +0000

    Added new project into prompt and refine folder

---
+++ AI-Workflows-Testing/Swot-It-Asking-And-Refining/Swot-It-Prompt-And-Refine/Swot-It-Prompt-And-Refine/Swot_It_Prompt_And_RefineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swot_It_Prompt_And_RefineApp.swift
+//  Swot-It-Prompt-And-Refine
+//
+//  Created by Michael Baldock on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swot_It_Prompt_And_RefineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
